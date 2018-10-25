b4490b8275fad64aee44edd446cf2d5e5b08092f
Ehlen, David <david.ehlen@dm.de>
2018-10-25 15:20:07 +0200

    added new target for macOS support

---
+++ cmark-gfm-swift-macOS/cmark_gfm_swift_macOS.h
@@ -0,0 +1,19 @@
+//
+//  cmark_gfm_swift_macOS.h
+//  cmark-gfm-swift-macOS
+//
+//  Created by Ehlen, David on 25.10.18.
+//  Copyright © 2018 Ryan Nystrom. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for cmark_gfm_swift_macOS.
+FOUNDATION_EXPORT double cmark_gfm_swift_macOSVersionNumber;
+
+//! Project version string for cmark_gfm_swift_macOS.
+FOUNDATION_EXPORT const unsigned char cmark_gfm_swift_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cmark_gfm_swift_macOS/PublicHeader.h>
+
+
